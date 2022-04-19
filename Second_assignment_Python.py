c2dca7c38f590aebe979223ac350a47f14da8d6a
Iryna Azimova <iryna.azimova@gmail.com>
2022-04-18 21:41:22 -0400

    Second_assignment_Python.py file got added

---
+++ Second_assignment_Python.py
@@ -0,0 +1,19 @@
+# Exercise 1
+my_string = "Test Automation"
+word_test = 'Test'
+print(word_test)
+
+# Exercise 2
+my_string = "Test Automation"
+second_word = "Automation"
+print(second_word)
+
+# Exercise 3
+my_list = [10,50,20,25,30]
+sliced_list = [10,25,30]
+print(sliced_list)
+
+
+
+
+
