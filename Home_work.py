688a9ccd5324ed13c1e9090b910e088f5a41a1e5
Iryna Azimova <iryna.azimova@gmail.com>
2022-04-20 04:00:34 -0400

    Home_work.py file got added

---
+++ Home_work.py
@@ -0,0 +1,15 @@
+#Exercise 1
+my_string = "Test Automation"
+word_test = "Test"
+print(word_test[0:5])
+
+#Exercise2
+my_string = "Test Automation"
+second_word = "Automation"
+print(second_word[0:10])
+
+
+#Exercise3
+my_list = [10,50,20,25,30]
+sliced_list = [50,20]
+print(sliced_list[0:3])
